0a88f5a99e364a65327d3d7ba71f7654d8a6ed58
liyu <307390819@qq.com>
2021-06-04 09:54:23 +0800

    配置webpack 插件

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+const CircularDependencyPlugin = require('circular-dependency-plugin')
+
+module.exports = {
+  chainWebpack(config) {
+    config.plugin('monaco').use(new MonacoWebpackPlugin())
+    config.plugin('circular').use(new CircularDependencyPlugin())
+  },
+}
