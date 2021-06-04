ed3af810ed6023ad9cd9c5cc83477986be9bd13a
liyu <307390819@qq.com>
2021-06-04 09:56:45 +0800

    sfc开发页面，存在问题

--- lib/fileds/StringField.vue
+++ lib/fileds/StringField.vue
@@ -1,9 +1,9 @@
 <template>
-  <input type="text" :value="value" />
+  <input type="text" :value="value" @input="handleChange" />
 </template>
 
 <script lang="ts" setup="props">
-import { ref, PropType } from 'vue'
+import { ref, PropType, defineProps, defineEmit } from 'vue'
 import { Schema } from '../types'
 
 export default {
@@ -27,6 +27,12 @@ declare const props: {
   onChange: (v: string) => void
   schame: Schema
 }
+// const props =
+//   defineProps<{
+//     value: any
+//     onChange: (v: string) => void
+//     schame: Schema
+//   }>()
 
 export const handleChange = (e: any) => {
   console.log(e)
