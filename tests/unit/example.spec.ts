1ccdd66ba849fabe87b3d33e52cdcd4e879ce505
liyu <307390819@qq.com>
2021-06-07 13:29:47 +0800

    配置单元测试

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,24 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { defineComponent, h } from 'vue'
+import { shallowMount } from '@vue/test-utils'
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
+const HelloWorld = defineComponent({
+  nanme: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  setup(props) {
+    return () => {
+      return h('div', props.msg)
+    }
+  },
+})
+
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
       props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
