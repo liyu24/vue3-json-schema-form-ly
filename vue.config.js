860cbc08e7fb1963005ea31c7b7bac6599e1490e
liyu <307390819@qq.com>
2021-06-09 17:25:46 +0800

    CI相关配置

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 const CircularDependencyPlugin = require('circular-dependency-plugin')
 
 const isLib = process.env.TYPE === 'lib'
+const isCI = process.env.CI
 
 module.exports = {
   chainWebpack(config) {
